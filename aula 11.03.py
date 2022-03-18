6254fe8685e0f46112443048fd2897cd349531e6
Lucas Pereira Coelho <101848408+LucasPereiraCoelho@users.noreply.github.com>
2022-03-17 22:51:52 -0300

    Add files via upload
    Exercícios feitos em aula.

---
+++ aula 11.03.py
@@ -0,0 +1,179 @@
+#Exemplos
+
+#nome = input("Informe seu nome completo: ")
+#idade = int(input("Informe sua idade: "))
+#altura = float(input("Informe sua altura: "))
+#peso = float(input("Informe seu peso: "))
+#print("Eu", nome, "tenho", idade, "anos. peso", peso, "Kg. tenho", altura, "de altura")
+
+
+
+#exercício 1 
+
+'''
+
+nome = input("Informe seu nome: ")
+idade = int(input("Informe sua idade: "))
+cpf = int(input("informe seu CPF: "))
+email = input("Informe seu email: ")
+
+print("Cadastro realizado com sucesso")
+
+print("Bem vindo,", nome, )
+
+'''
+
+
+
+#Exercicio 2 
+
+'''
+
+soma = 5 + 2
+print(soma)
+
+subtracao = 5 - 2
+print(subtracao)
+
+multiplicacao = 5 * 2
+print(multiplicacao)
+
+divisao = (10 / 2)
+print(divisao)
+
+mod = 5 % 2
+print(mod)
+
+
+calculo1 = 10 + 2 * 10
+calculo2 = (10 + 2) * 10
+
+print(calculo1)
+print(calculo2)
+
+'''
+
+
+
+#Continuação exercício 2 
+
+'''
+
+valor1 = int(input("Informe o primeiro valor: "))
+valor2 = int(input("Informe o segundo valor: "))
+
+soma = valor1 + valor2
+print("soma", soma)
+
+subtracao = valor1 - valor2
+print("subtração", subtracao) 
+
+multiplicacao = valor1 * valor2
+print("multiplicação", multiplicacao)
+
+divisao = int(valor1 / valor2)
+print("divisão", divisao)
+
+mod = valor1 % valor2
+print("resto da divisão", mod)
+
+'''
+
+
+
+#Exercício 3 
+
+'''
+
+qtdcavalos = int(input("informe o numero de cavalos:"))
+
+multiplicacao = qtdcavalos * 4
+
+print("para", qtdcavalos, "cavalos, sao necessarios", multiplicacao, "de ferraduras")
+
+'''
+
+
+
+#Exercício 4 
+
+'''
+
+pao = int(input("Informe a quantidade de pães vendidos: "))
+broa = int(input("Informe a quantidade de broas vendidas: "))
+
+multpao = pao * 0.12
+multbroa = broa * 1.5
+
+lucro = multpao + multbroa 
+print("arrecadou ao final do dia", lucro, "reais")
+
+poupanca = lucro * 0.1 
+print("deve guardar",  poupanca, "em sua poupança")
+
+
+'''
+
+
+
+#Exercício 5
+
+'''
+
+nome = input("Informe seu nome: ")
+idade = int(input("Informe sua idade: "))
+
+vida = idade * 365
+
+print("Parabéns", nome, "você ja viveu", vida, "dias.")
+ 
+'''
+
+
+
+#Exercício 6 
+
+'''
+
+valorprato = float(input("Informe o peso do prato em KG: "))
+
+total = valorprato * 12
+
+print ("Total a pagar:", total, "reais")
+
+'''
+
+
+
+#Exercício 7 
+
+'''
+
+mes = int(input("Informe o mês: "))
+dia = int(input("informe o dia: "))
+
+print("Já se passaram", (mes - 1) * 30 + dia, "dias")
+
+'''
+
+
+
+#Exercício 8 
+
+'''
+
+salario = float(input("Informe o seu salário: "))
+
+aumentosalario = salario * 0.15
+imposto =  aumentosalario * 0.08
+
+print("Salário inicial: ", salario)
+print("Salario com aumento: ", aumentosalario + salario)
+print("Salário final: ", (aumentosalario + salario) - imposto )
+
+'''
+
+
+
+
+
